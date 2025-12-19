d7abb3243777d2d016d2f178eb6d39dcca051dc5
lorrainemakuyana <lorrainem.dev@gmail.com>
2025-12-19 15:42:27 +0000

    removed unnecessary files

--- src/lib/utils/clsxm.ts
+++
@@ -1,6 +0,0 @@
-import { twMerge } from "tailwind-merge";
-import clsx, { ClassValue } from "clsx";
-
-export default function clsxm(...classes: ClassValue[]) {
-  return twMerge(clsx(...classes));
-}

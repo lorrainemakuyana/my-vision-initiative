8a763e2b57cd285362c1288631ada16803f2115e
lorrainemakuyana <lorrainem.dev@gmail.com>
2025-12-19 16:06:38 +0000

    refactor(Footer): Simplify structure and improve styling for consistency

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -12,49 +12,41 @@ import { MdOutlineEmail } from "react-icons/md";
 
 function Footer() {
   return (
-    <section className="border-t border-magenta pb-8 pt-5">
-      <div className="mx-auto flex w-4/5 flex-row items-center justify-center lg:justify-between">
-        <div className="text-sm text-magenta">
-          <p>&copy;{new Date().getFullYear()} My Vision Initiative</p>
-          <p>
-            Designed and built by{" "}
-            <a href="https://www.linkedin.com/in/lorraine-makuyana/">
-              Lorraine Makuyana
-            </a>
-          </p>
-        </div>
+    <footer className="border-t-magenta border-t bg-white p-5 text-center text-base text-black">
+      <p>&copy;{new Date().getFullYear()} My Vision Initiative</p>
+    </footer>
+  );
+
+  return (
+    <section className="border-magenta border-t p-5 text-sm text-black">
+      <div className="mx-auto flex w-full max-w-7xl flex-row items-center justify-center lg:justify-between">
         <div className="hidden flex-row space-x-10 lg:flex">
           <a
             href="https://web.facebook.com/myvisioninitiative/"
             target="_blank"
           >
-            <FaFacebook className="h-6 w-6 text-magenta" />
-          </a>
-          <a href="mailto:myvisioninitiative@gmail.com">
-            <MdOutlineEmail className="h-6 w-6 text-magenta" />
-          </a>
-          <a href="https://twitter.com/mvi_initiative" target="_blank">
-            <FaXTwitter className="h-6 w-6 text-magenta" />
+            <FaFacebook className=" h-6 w-6" />
           </a>
           <a
             href="https://www.instagram.com/myvisioninitiative/"
             target="_blank"
           >
-            <FaInstagram className="h-6 w-6 text-magenta" />
+            <FaInstagram className=" h-6 w-6" />
           </a>
           <a
             href="https://www.linkedin.com/company/my-vision-initiative/"
             target="_blank"
           >
-            <FaLinkedin className="h-6 w-6 text-magenta" />
+            <FaLinkedin className=" h-6 w-6" />
           </a>
           <a
             href="https://www.youtube.com/channel/UC2doZCOhujqVWHP3DoamDwg"
             target="_blank"
           >
-            <FaYoutube className="h-6 w-6 text-magenta" />
+            <FaYoutube className=" h-6 w-6" />
           </a>
         </div>
+        <p>&copy;{new Date().getFullYear()} My Vision Initiative</p>
       </div>
     </section>
   );

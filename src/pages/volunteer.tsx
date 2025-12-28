5c39c878330dd383f150ffaf3b1bae88f9cafb28
lorrainemakuyana <lorrainem.dev@gmail.com>
2025-12-28 19:53:51 +0000

    feat(volunteer) page

---
+++ src/pages/volunteer.tsx
@@ -0,0 +1,15 @@
+import PageBanner from "@/components/shared/PageBanner";
+import React from "react";
+
+function volunteer() {
+  return (
+    <>
+      <PageBanner
+        title="Volunteer with Us"
+        subtitle="Join us in making a difference in the lives of young women and girls in Zimbabwe. Your time and skills can help empower the next generation! To volunteer, please reach out to us via our contact details below."
+      />
+    </>
+  );
+}
+
+export default volunteer;

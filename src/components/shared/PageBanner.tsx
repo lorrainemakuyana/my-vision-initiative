2a1547b5ed8ac9b32f32f1a163c8a7bb5c71ac54
lorrainemakuyana <lorrainem.dev@gmail.com>
2025-12-28 19:41:59 +0000

    fixes for page banner

--- src/components/shared/PageBanner.tsx
+++ src/components/shared/PageBanner.tsx
@@ -4,11 +4,9 @@ function PageBanner({ title, subtitle }: { title: string; subtitle?: string }) {
   return (
     <div className="bg-inherit pb-16 pt-36">
       <div className="container mx-auto px-4">
-        <h1 className="text-magenta text-center text-4xl font-bold">
-          {title}
-        </h1>
+        <h1 className="text-magenta text-center text-4xl font-bold">{title}</h1>
         {subtitle && (
-          <p className="mt-4 text-center text-lg text-gray-500 lg:mx-auto lg:w-1/2">
+          <p className="mt-4 text-center text-lg font-light text-gray-500 lg:mx-auto lg:w-1/2">
             {subtitle}
           </p>
         )}
